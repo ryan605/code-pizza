9d5db9f4018cf243c2081f684d9705170c07e51a
ryan605 <langatryan26@gmail.com>
2023-03-21 10:12:22 +0300

    run

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,5 +1,5 @@
 class RestaurantPizza < ApplicationRecord
-    validates :price, presence: true, length: {minimum: 1 , maximum: 30}
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 , less_than_or_equal_to: 30}
     belongs_to :pizza
     belongs_to :restaurant
 end
--- db/migrate/20230319062613_add_restaurant_and_pizza_to_restaurant_pizza.rb
+++ db/migrate/20230319062613_add_restaurant_and_pizza_to_restaurant_pizza.rb
@@ -1,6 +1,6 @@
 class AddRestaurantAndPizzaToRestaurantPizza < ActiveRecord::Migration[7.0]
   def change
-    add_column :restaurant_pizzas, :restaurant_id, :integer :null => false
-    add_column :restaurant_pizzas, :pizza_id, :integer :null => false
+    add_column :restaurant_pizzas, :restaurant_id, :integer
+    add_column :restaurant_pizzas, :pizza_id, :integer 
   end
 end
