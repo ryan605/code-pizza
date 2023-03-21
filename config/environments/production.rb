862635bcea68a0707514e22c3f01c0e22b7ee8e4
ryan605 <langatryan26@gmail.com>
2023-03-21 09:52:33 +0300

    render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_record.sqliye3_production_warning = false
   # Code is not reloaded between requests.
   config.cache_classes = true
 
